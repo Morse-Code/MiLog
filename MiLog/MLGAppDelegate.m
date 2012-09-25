7d3faa4696d34757a57c465f84109e8533e219ec
Christopher Morse <dignanmorse@gmail.com>
2012-09-25 09:54:31 -0400

    Add proper cleanup and save state methods to AppDelegate.
    - Destroy NSTimer object when entering background.
    - save NSManagedObjectContext to persistent store when entering background and resigning activity.
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- MiLog/MLGAppDelegate.m
+++ MiLog/MLGAppDelegate.m
@@ -37,14 +37,15 @@
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    UINavigationController *navigationController = (UINavigationController *) self.window.rootViewController;
+    MLGMasterViewController *controller = (MLGMasterViewController *) navigationController.topViewController;
+    [self saveContext];
+    controller.pollingTimer = nil;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self saveContext];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -59,7 +60,6 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Saves changes in the application's managed object context before the application terminates.
     [self saveContext];
 }
 
