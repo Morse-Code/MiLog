8b56b1732cb94a76379a2cae71c403f4018802c7
Christopher Morse <dignanmorse@gmail.com>
2012-09-25 10:09:29 -0400

    Convenience methods added to custom Core Data model entity class.
    - public class method to alloc and add new TimerEvent into passed managedObjectContext.
    - private convenience method to return entity name.
    - public convenience method for updating entity attributes using passed NSDate instance.
    - public method to update entity attributes using passed NSDate instance, and state.
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- MiLog/TimerEvent.h
+++ MiLog/TimerEvent.h
@@ -12,11 +12,26 @@
 
 @interface TimerEvent : NSManagedObject
 
-@property (nonatomic, retain) NSString * name;
-@property (nonatomic, retain) NSString * note;
-@property (nonatomic) NSTimeInterval start;
-@property (nonatomic) NSTimeInterval stop;
-@property (nonatomic) int16_t state;
-@property (nonatomic) NSTimeInterval timeStamp;
+
+
+@property(nonatomic, retain) NSString *name;
+@property(nonatomic, retain) NSString *note;
+@property(nonatomic, retain) NSString *timeString;
+@property(nonatomic, retain) NSDate *start;
+@property(nonatomic, retain) NSDate *stop;
+@property(nonatomic) int16_t state;
+@property(nonatomic) NSDate *timeStamp;
+@property(nonatomic) NSTimeInterval elapsed;
+@property(nonatomic, retain) NSString *sectionName;
+
+
++ (TimerEvent *)addEventToContext:(NSManagedObjectContext *)context;
+
++ (NSString *)entityName;
+
+- (NSTimeInterval)setTimeIntervalToDate:(NSDate *)date;
+
+- (void)setElapsedForStopDate:(NSDate *)date
+                    withState:(int16_t)state;
 
 @end
--- MiLog/TimerEvent.m
+++ MiLog/TimerEvent.m
@@ -8,14 +8,103 @@
 
 #import "TimerEvent.h"
 
+#define NEW 0
+#define ACTIVE 1
+#define PAUSE 2
+#define HISTORY 3
+
+@interface TimerEvent ()
+
+
+
+@property(nonatomic) NSTimeInterval elapsedAlias;
+
+- (NSString *)timeStringForInterval:(NSTimeInterval)interval;
+
+@end
 
 @implementation TimerEvent
 
+
+
 @dynamic name;
 @dynamic note;
+@dynamic timeString;
 @dynamic start;
 @dynamic stop;
 @dynamic state;
 @dynamic timeStamp;
+@synthesize elapsedAlias = elapsed;
+@dynamic sectionName;
+
+
++ (TimerEvent *)addEventToContext:(NSManagedObjectContext *)context
+{
+    NSEntityDescription *entity = [NSEntityDescription entityForName:[TimerEvent entityName]
+                                              inManagedObjectContext:context];
+    NSAssert1(entity != nil, @"The entity description for TimerEvent in %@ is nil", context);
+    TimerEvent *entry = [[TimerEvent alloc] initWithEntity:entity
+                            insertIntoManagedObjectContext:context];
+    entry.timeStamp = [NSDate date];
+    entry.state = NEW;
+    entry.elapsed = 0.0;
+    entry.stop = nil;
+    entry.timeString = [entry timeStringForInterval:entry.elapsed];
+    entry.sectionName = @"Active Timers";
+    NSError *error = nil;
+    if (![context save:&error]) {
+        [NSException raise:NSInternalInconsistencyException format:@"An error occured when saving the context: %@",
+                                                                   [error localizedDescription]];
+    }
+    return entry;
+}
+
++ (NSString *)entityName
+{
+    return @"TimerEvent";
+}
+
+
+- (void)setElapsed:(NSTimeInterval)interval
+{
+    self.elapsedAlias = interval;
+}
+
+- (NSTimeInterval)elapsed
+{
+    return self.elapsedAlias;
+}
+
+- (NSTimeInterval)setTimeIntervalToDate:(NSDate *)date
+{
+    NSTimeInterval interval = [date timeIntervalSinceDate:self.start];
+    interval += self.elapsed;
+    self.timeString = [self timeStringForInterval:interval];
+    return interval;
+}
+
+- (NSString *)timeStringForInterval:(NSTimeInterval)interval
+{
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:interval];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    [dateFormatter setDateFormat:@"HH:mm:ss.SSS"];
+    NSString *time = [dateFormatter stringFromDate:date];
+    return time;
+}
+
+- (void)setElapsedForStopDate:(NSDate *)date
+                    withState:(int16_t)state
+{
+    if (state == HISTORY) {
+        self.sectionName = @"History";
+    }
+    else {
+        self.sectionName = @"Active Timers";
+    }
+    self.state = state;
+    self.stop = date;
+    self.elapsed = [self setTimeIntervalToDate:date];
+}
 
 @end
